4433dc58f9aa8eb5211d0fd805eef5ad582cc67b
Bob Sullivan <robertemmettsullivan@gmail.com>
2024-07-18 15:24:57 -0400

    updated Google Strategy to use client id and secret

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 // common js module import syntax - nodeJS does not have support for ES2015 modules yet
 const express = require('express');
 const passport = require('passport');
+const keys = require('./config/keys');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 
-passport.use(new GoogleStrategy());
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, (accessToken) => {
+        console.log(accessToken);
+    })
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
