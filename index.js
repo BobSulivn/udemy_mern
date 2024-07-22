b41b9c025b3076dd32e19a76b4275072130a2424
Bob Sullivan <robertemmettsullivan@gmail.com>
2024-07-21 21:23:52 -0400

    refactor

--- index.js
+++ index.js
@@ -1,34 +1,11 @@
 // common js module import syntax - nodeJS does not have support for ES2015 modules yet
 const express = require("express");
-const passport = require("passport");
-const keys = require("./config/keys");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
+require("./services/passport");
 
 const app = express();
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback",
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log("access token", accessToken);
-      console.log("refresh token", refreshToken);
-      console.log("profile", profile);
-    }
-  )
-);
-
-app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"],
-  })
-);
-
-app.get("/auth/google/callback", passport.authenticate("google"));
+// authRoutes returns a function that we immediately call with the express app created above
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
