55813f479ec063ec5410a1f89e6cacdf5105901a
Bob Sullivan <robertemmettsullivan@gmail.com>
2024-08-21 10:27:27 -0400

    set up app to use cookies

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // common js module import syntax - nodeJS does not have support for ES2015 modules yet
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const keys = require("./config/keys");
 require("./models/User");
 require("./services/passport");
@@ -8,6 +10,17 @@ require("./services/passport");
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+  cookieSession({
+    // set cookie age to be 30 days (converted to milliseconds)
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // authRoutes returns a function that we immediately call with the express app created above
 require("./routes/authRoutes")(app);
